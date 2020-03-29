2aae3adf8aabfbd21cefd9be748a6cec9696c008
Raja Doake <rdoake@gmail.com>
2020-03-28 19:58:27 -0500

    Integrate with cobr.ai to get the weekly pairings.

--- R/cobra.R
+++ R/cobra.R
@@ -4,6 +4,7 @@ library(jsonlite)
 cobra_link <- "http://cobr.ai/tournaments/1628.json"
 
 cobra_stats <- fromJSON(cobra_link)
+names(cobra_stats$rounds) <- paste("Week", 1:length(cobra_stats$rounds))
 
 # TODO make a sidebar dropdown that lists the weeks to date and then displays that week's pairings
 # and unpaired players. then this function can take the week from the UI and return a table of 
--- app.R
+++ app.R
@@ -1,4 +1,6 @@
 library(shiny)
+library(dplyr)
+library(purrr)
 
 # Store modules in /R so that future versions will autoload them
 source("R/data.R")
@@ -10,13 +12,21 @@ source("R/league_links.R")
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Greater Twin Cities Netrunner League Points"),
+    titlePanel("Greater Twin Cities Netrunner League"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
             
             actionButton("update", "Update Standings", class = "btn-primary"),
+            
+            br(),
+            br(),
+            br(),
+            
+            selectInput("selected_pairings", "Pairing Week", 
+                        choices = names(cobra_stats$rounds), 
+                        selected = last(names(cobra_stats$rounds))),
             
             br(),
             br(),
@@ -39,7 +49,14 @@ ui <- fluidPage(
         # Show a plot of the generated distribution
         mainPanel(
             
-            tableOutput("league_table")
+            h4("League Standings"),
+            tableOutput("league_table"),
+            
+            br(),
+            br(),
+
+            h4("Weekly Pairings"),            
+            tableOutput("pairings")
             
         )
     )
@@ -52,6 +69,26 @@ server <- function(input, output) {
     league_data <- eventReactive({input$action | input$update}, loadData())
     
     output$league_table <- renderTable(league_data())
+    
+    # TODO put this in a different column or something once it's working, or maybe have tabs to
+    # choose from displaying standings, pairings, or plots
+    output$pairings <- renderTable({
+        
+        get_player_name <- function(p) {
+            
+            filter(cobra_stats$players, id == p) %>% 
+            select(id, name)
+            
+        }
+
+        p1 <- map_dfr(cobra_stats$rounds[[input$selected_pairings]]$player1$id, get_player_name)
+        
+        p2 <- map_dfr(cobra_stats$rounds[[input$selected_pairings]]$player2$id, get_player_name)
+ 
+        data.frame(Player1 = p1$name,
+                   Player2 = p2$name)
+                
+    })
     
 }
 
