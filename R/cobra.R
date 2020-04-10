59610e423d25e9fc714d2ee4a6398fa91e166c4b
Raja Doake <rdoake@gmail.com>
2020-04-09 21:38:41 -0500

    Handle a bye on the left or right of a pairing.

--- R/cobra.R
+++ R/cobra.R
@@ -7,19 +7,24 @@ names(cobra_stats$rounds) <- paste("Week", 1:length(cobra_stats$rounds))
 
 get_player_name <- function(p, stats = cobra_stats) {
 
-    filter(stats$players, id == p) %>%
-        select(id, name)
+    name <-
+        filter(stats$players, id == p) %>%
+        select(name) %>%
+        as.character()
+
+    # TODO coercing empty to character to NA to exploit that later is kind of hacky
+    ifelse(name == "character(0)", NA, name)
 
 }
 
 
 league_pairings <- function(selected_pairings, stats = cobra_stats) {
 
-    p1 <- map_dfr(stats$rounds[[selected_pairings]]$player1$id, get_player_name)
+    p1 <- map_chr(stats$rounds[[selected_pairings]]$player1$id, get_player_name)
 
-    p2 <- map_dfr(stats$rounds[[selected_pairings]]$player2$id, get_player_name)
+    p2 <- map_chr(stats$rounds[[selected_pairings]]$player2$id, get_player_name)
 
-    tibble(Player1 = p1$name,
-           Player2 = c(p2$name, rep(NA, times = nrow(p1) - nrow(p2))))
+    tibble(Player1 = p1,
+           Player2 = p2)
 
 }
