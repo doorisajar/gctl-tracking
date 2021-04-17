69deb75abb821d9f281d52ccb5a1ad6a0fb7568c
Raja Doake <rdoake@gmail.com>
2021-04-17 13:51:41 -0500

    Make the dummy "zeroth report" smarter about where it appears.

--- R/plots.R
+++ R/plots.R
@@ -12,9 +12,7 @@ daily_plot <- function(data, params) {
         group_by(id, report_date) %>%
         summarize(cumulative_points = max(cumulative_points, na.rm = TRUE))
 
-    players <- map_dfr(unique(data$id), function(id) list(id = id,
-                                                          report_date = ymd_hms(params$league_start),
-                                                          cumulative_points = 0))
+    players <- map_dfr(unique(data$id), zeroth_report, data, params)
 
     data <- bind_rows(players, data)
 
@@ -28,5 +26,38 @@ daily_plot <- function(data, params) {
         ylab("Cumulative League Points")
 
     ggplotly(static_plot)
+
+}
+
+
+# a player's first appearance on the plot is either at 0 points on the start of the league week,
+# or at whatever points they had from games reported on the league start date.
+zeroth_report <- function(id, data, params) {
+
+    id_reports <- filter(data, id == {{id}})
+
+    zr <- list(id = NULL,
+               report_date = NULL,
+               cumulative_points = NULL)
+
+    if (nrow(id_reports) > 0) {
+
+        league_start <- ymd_hms(params$league_start)
+        first_report <- min(id_reports$report_date)
+
+        if (first_report > league_start) {
+
+            # add 0 weeks for league week 1, 1 week for league week 2, etc.
+            week_start <- league_start + dweeks(league_week(first_report, league_start) - 1)
+
+            zr <- list(id = id,
+                       report_date = week_start,
+                       cumulative_points = 0)
+
+        }
+
+    }
+
+    zr
 
 }
